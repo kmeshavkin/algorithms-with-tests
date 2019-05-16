fce806433a216cb07bfb78b2bb6d0ca2b39453e3
Kostyan1996 <meshavkin1996@gmail.com>
2019-05-17 00:53:33 +0300

    Added JSDocs + couple of tests

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,8 @@
+/**
+ * Returns true if number is prime, otherwise returns false.
+ * @param {Number} n Number to check.
+ * @return {boolean} Returns if number is prime or not.
+ */
 export function isPrime(n) {
   if (n < 4) return false;
   for (let i = 2; i <= n / 2; i++) {
@@ -6,8 +11,13 @@ export function isPrime(n) {
   return false;
 }
 
+/**
+ * Returns all Prime Factors from number
+ * @param {Number} n Number to get all Prime Factors from
+ * @return {Array} The array that contains all Prime Factors
+ */
 export function getPrime(n) {
-  let primeArr = [];
+  const primeArr = [];
   for (let i = 2; i <= n / 2; i++) {
     if (n % i == 0) primeArr.push(i);
   }
--- algorithms.test.js
+++ algorithms.test.js
@@ -1,6 +1,7 @@
-import { isPrime, getPrime } from './algorithms';
+import {isPrime, getPrime, fibonacci} from './algorithms';
 
 describe('isPrime', () => {
+  test(`not number is NOT a prime`, () => expect(isPrime('abc')).toBe(false));
   test(`0 is NOT a prime`, () => expect(isPrime(0)).toBe(false));
   test(`1 is NOT a prime`, () => expect(isPrime(1)).toBe(false));
   test(`negative number is NOT a prime`, () => expect(isPrime(-4)).toBe(false));
@@ -9,6 +10,7 @@ describe('isPrime', () => {
 });
 
 describe('getPrime', () => {
+  test(`not number returns no primes`, () => expect(getPrime('abc')).toEqual([]));
   test(`3 has no primes`, () => expect(getPrime(3)).toEqual([]));
   test(`4 has prime [2]`, () => expect(getPrime(4)).toEqual([2]));
   test(`24 has primes [2, 3, 4, 6, 8, 12]`, () => expect(getPrime(24)).toEqual([2, 3, 4, 6, 8, 12]));
